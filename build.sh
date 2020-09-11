f7434f5509834cfc4a600e19219daf27f3675eae
notthensa <mjthomps@protonmail.com>
2020-09-10 22:29:02 -0500

    tests passing

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir $DIR/bin || true
-(cd $DIR/bin && aarch64-linux-gnu-as ../ms.S -o ms.o && aarch64-linux-gnu-gcc --static ../main.c ms.o -o ms) | tee /dev/tty
+(cd $DIR/bin && aarch64-linux-gnu-as ../ms.S -o ms.o && aarch64-linux-gnu-gcc --static -g3 ../main.c ms.o -o ms) | tee /dev/tty
--- main.c
+++ main.c
@@ -4,70 +4,83 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-extern uint8_t* mergeSort( uint8_t* buf, size_t len );
-extern uint8_t* merge( uint8_t* bufm, size_t lenm, uint8_t* bufn, size_t lenn );
+extern void mergeSort( uint8_t* buf, size_t len, uint8_t* res );
+extern void merge( uint8_t* bufm, size_t lenm, uint8_t* bufn, size_t lenn, uint8_t* bufr );
+extern void retard( uint8_t bufm, size_t lenm, uint8_t bufn );
 int cmp( uint8_t* res, uint8_t* exp, size_t len );
+void printArr( uint8_t* arr, size_t len );
 
 int main()
 {
+   int i=1;
+   int j=2;
+   int k=3;
+
+   retard(i,j,k);
 
    // merge() tests
    {
+      printf("Test 1");
       uint8_t m[] = { 1 };
       uint8_t n[] = { 1 };
       size_t mlen = sizeof(m) / sizeof(uint8_t);
       size_t nlen = sizeof(n) / sizeof(uint8_t);
 
       uint8_t exp[] = { 1, 1 };
-      uint8_t* res = merge( m, mlen, n, nlen );
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
       assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
-      free(res);
    }
    {
+      printf("Test 2");
       uint8_t m[] = { 1 };
       uint8_t n[] = { 2 };
       size_t mlen = sizeof(m) / sizeof(uint8_t);
       size_t nlen = sizeof(n) / sizeof(uint8_t);
 
       uint8_t exp[] = { 1, 2 };
-      uint8_t* res = merge( m, mlen, n, nlen );
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
       assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
-      free(res);
    }
    {
+      printf("Test 3");
       uint8_t m[] = { 2 };
       uint8_t n[] = { 1 };
       size_t mlen = sizeof(m) / sizeof(uint8_t);
       size_t nlen = sizeof(n) / sizeof(uint8_t);
 
       uint8_t exp[] = { 1, 2 };
-      uint8_t* res = merge( m, mlen, n, nlen );
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
       assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
-      free(res);
    }
    {
+      printf("Test 4");
       uint8_t m[] = { 2, 4 };
       uint8_t n[] = { 1, 3 };
       size_t mlen = sizeof(m) / sizeof(uint8_t);
       size_t nlen = sizeof(n) / sizeof(uint8_t);
 
       uint8_t exp[] = { 1, 2, 3, 4 };
-      uint8_t* res = merge( m, mlen, n, nlen );
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
       assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
-      free(res);
    }
    {
+      printf("Test 5");
       uint8_t m[] = { 2 };
       uint8_t n[] = { 1, 3 };
       size_t mlen = sizeof(m) / sizeof(uint8_t);
       size_t nlen = sizeof(n) / sizeof(uint8_t);
 
       uint8_t exp[] = { 1, 2, 3 };
-      uint8_t* res = merge( m, mlen, n, nlen );
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
       assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
-      free(res);
    }
    {
+      printf("Test 6");
       uint8_t m[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
       uint8_t n[] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
       size_t mlen = sizeof(m) / sizeof(uint8_t);
@@ -75,53 +88,93 @@ int main()
 
       uint8_t exp[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
          11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
-      uint8_t* res = merge( m, mlen, n, nlen );
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
       assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
-      free(res);
    }
    {
+      printf("Test 7");
       uint8_t m[] = { 1 };
       uint8_t n[] = { 2, 3, 4, 5, 6, 7 };
       size_t mlen = sizeof(m) / sizeof(uint8_t);
       size_t nlen = sizeof(n) / sizeof(uint8_t);
 
       uint8_t exp[] = { 1, 2, 3, 4, 5, 6, 7 };
-      uint8_t* res = merge( m, mlen, n, nlen );
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
       assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
-      free(res);
+   }
+   {
+      printf("Test 8");
+      uint8_t m[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+      uint8_t n[] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+      size_t mlen = sizeof(m) / sizeof(uint8_t);
+      size_t nlen = sizeof(n) / sizeof(uint8_t);
+
+      uint8_t exp[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+      uint8_t mexp[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+      uint8_t nexp[] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+      uint8_t res[((sizeof(m) + sizeof(n))/sizeof(uint8_t))] = {};
+      merge( m, mlen, n, nlen, res );
+      assert(cmp(res, exp, (sizeof(exp) / sizeof(uint8_t))));
+      assert(cmp(m, mexp, (sizeof(mexp) / sizeof(uint8_t))));
+      assert(cmp(n, nexp, (sizeof(nexp) / sizeof(uint8_t))));
    }
    // merge sort tests
    {
-      uint8_t test[] = { 2, 1 };
-      size_t len = sizeof(test) / sizeof(uint8_t);
+      printf("Test 8");
+      uint8_t test[] = { 45 };
+      const size_t len = sizeof(test) / sizeof(uint8_t);
 
-      uint8_t exp[] = { 1, 2 };
-      uint8_t* res = mergeSort( test, len );
+      uint8_t exp[] = { 45 };
+      uint8_t res[len];
+      mergeSort( test, len, res );
       assert(cmp(res, exp, len));
-      free(res);
    }
    {
+      printf("Test 9");
+      uint8_t test[] = { 2, 1 };
+      const size_t len = sizeof(test) / sizeof(uint8_t);
+
+      uint8_t exp[] = { 1, 2 };
+      uint8_t res[len];
+      mergeSort( test, len, res );
+      assert(cmp(res, exp, len));
+   }
+
+   {
+      printf("Test 10");
+      uint8_t test[] = { 2, 4, 1, 3 };
+      const size_t len = sizeof(test) / sizeof(uint8_t);
+
+      uint8_t exp[] = { 1, 2, 3, 4};
+      uint8_t res[len];
+      mergeSort( test, len, res );
+      assert(cmp(res, exp, len));
+   }
+   {
+      printf("Test 11");
       uint8_t test[] = { 4, 3, 2, 1 };
       size_t len = sizeof(test) / sizeof(uint8_t);
 
       uint8_t exp[] = { 1, 2, 3, 4 };
-      uint8_t* res = mergeSort( test, len );
+      uint8_t res[len];
+      mergeSort( test, len, res );
       assert(cmp(res, exp, len));
-      free(res);
    }
 
-//   {
-//      uint8_t test[] = { 4, 3, 2, 1 };
-//      mergeSort( test, (sizeof(test) / sizeof(uint8_t)) );
-//      uint8_t exp[] = { 1, 2, 3, 4 };
-//      assert(cmp(test, exp, (sizeof(test) / sizeof(uint8_t))));
-//   }
-//
-//   {
-//      char test[] = "83474321";
-//      char exp[] = "12334478";
-//      assert(cmp(test, exp, (sizeof(test) / sizeof(char))));
-//   }
+   {
+      printf("Test 12");
+      uint8_t test[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+      size_t len = sizeof(test) / sizeof(uint8_t);
+
+      uint8_t exp[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+         11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+      uint8_t res[len];
+      mergeSort( test, len, res );
+      assert(cmp(res, exp, len ));
+   }
 }
 
 int cmp( uint8_t* res, uint8_t* exp, size_t len )
@@ -130,10 +183,25 @@ int cmp( uint8_t* res, uint8_t* exp, size_t len )
    {
       if( res[i] != exp[i] )
       {
-         printf("Test fail \n");
+         printf("\t:: fail \n");
+         printf("Result: ");
+         printArr( res, len );
+         printf("\n");
+         printf("Expect: ");
+         printArr( exp, len );
+         fflush(stdout);
          return false;
       }
    }
-   printf("Test pass \n");
+   printf("\t:: pass \n");
    return true;
+}
+
+void printArr( uint8_t* arr, size_t len )
+{
+   for( size_t i=0; i<len; i++ )
+   {
+      printf("%d ", arr[i]);
+   }
+
 }
