196c106046acb65fc5c379e8f954dd04b1834933
notthensa <mjthomps@protonmail.com>
2020-09-13 16:27:37 -0500

    added test for longer array

--- main.c
+++ main.c
@@ -6,17 +6,11 @@
 
 extern void mergeSort( uint8_t* buf, size_t len, uint8_t* res );
 extern void merge( uint8_t* bufm, size_t lenm, uint8_t* bufn, size_t lenn, uint8_t* bufr );
-extern void retard( uint8_t bufm, size_t lenm, uint8_t bufn );
 int cmp( uint8_t* res, uint8_t* exp, size_t len );
 void printArr( uint8_t* arr, size_t len );
 
 int main()
 {
-   int i=1;
-   int j=2;
-   int k=3;
-
-   retard(i,j,k);
 
    // merge() tests
    {
@@ -169,8 +163,24 @@ int main()
       uint8_t test[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
       size_t len = sizeof(test) / sizeof(uint8_t);
 
+      uint8_t exp[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+      uint8_t res[len];
+      mergeSort( test, len, res );
+      assert(cmp(res, exp, len ));
+   }
+
+   {
+      printf("Test 13");
+      uint8_t test[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
+         1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
+         22, 24, 26, 28, 30, 32, 34, 36, 38, 40,
+         21, 23, 25, 27, 29, 31, 33, 35, 37, 39 };
+      size_t len = sizeof(test) / sizeof(uint8_t);
+
       uint8_t exp[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-         11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+         11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+         21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+         31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
       uint8_t res[len];
       mergeSort( test, len, res );
       assert(cmp(res, exp, len ));
